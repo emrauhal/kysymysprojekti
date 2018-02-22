97bf12ffaffa1c208fc256460f2f711cefe3153c
Elina <elina0rauhala@gmail.com>
2018-02-22 15:12:48 +0200

    pientä pilkkomista

--- src/KayttoLiittyma.java
+++ src/KayttoLiittyma.java
@@ -1,4 +1,3 @@
-import jdk.nashorn.internal.runtime.OptimisticReturnFilters;
 
 import java.sql.*;
 import java.util.*;
@@ -18,7 +17,7 @@ public class KayttoLiittyma {
             Connection con = DriverManager.
                     getConnection("jdbc:mysql://localhost:3306/kysymykset?useSSL=false",
                             "root", "Academy18");
-            tulostaKysymys(con);
+            kysely(con);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -27,11 +26,7 @@ public class KayttoLiittyma {
         System.out.println("Kiitos käynnistä ja tervetuloa uudelleen!");
     }
 
-    private static void tulostaKysymys(Connection con) throws SQLException {
-        //Random randomi  = new Random ();
-        String sql = "SELECT teksti FROM kysymys WHERE id=?";
-        PreparedStatement lause = con.prepareStatement(sql);
-        //int i = randomi.nextInt(3) + 1;
+    private static void kysely(Connection con) throws SQLException {
         List<Integer> lista = new ArrayList<>();
         lista.add(1);
         lista.add(2);
@@ -43,29 +38,11 @@ public class KayttoLiittyma {
         lista.add(8);
         lista.add(9);
         lista.add(10);
-
-
         Collections.shuffle(lista);
 
         for (int i = 1; i <= lista.size(); i++) {
-            lause.setInt(1, i);
-            ResultSet rs = lause.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-
-            String sql2 = "SELECT * FROM kysymysvaihtoehdot WHERE kysymysID=?";
-            PreparedStatement lause2 = con.prepareStatement(sql2);
-            lause2.setInt(1, i);
-            ResultSet rs2 = lause2.executeQuery();
-            int k = 1;
-            int oikea = 0;
-            while (rs2.next()) {
-                System.out.println(k + ". " + rs2.getString(2));
-                if (rs2.getInt("oikeavastaus") == 1)
-                    oikea = k;
-                k++;
-            }
+            tulostaKysymys(con, i);
+            int oikea = tulostaKysymysvaihtoehdot(con, i);
 
             Scanner lukija = new Scanner(System.in);
             System.out.print("Anna vastaus: ");
@@ -79,10 +56,12 @@ public class KayttoLiittyma {
                         break;
                     } else {
                         System.out.println("Syöte ei kelpaa!");
+                        System.out.print("Anna vastaus: ");
                         continue;
                     }
                 } else {
                     System.out.println("Tyhjä syöte!");
+                    System.out.print("Anna vastaus: ");
                     continue;
                 }
             }
@@ -95,8 +74,33 @@ public class KayttoLiittyma {
                 System.out.println("Väärin!");
                 System.out.println("Oikea vaihtoehto olisi ollut: "+ oikea);
                 System.out.println("\n");
-
             }
         }
+    }
+
+    private static void tulostaKysymys(Connection con, int kysymyksenIndeksi) throws SQLException {
+        String sql = "SELECT teksti FROM kysymys WHERE id=?";
+        PreparedStatement lause = con.prepareStatement(sql);
+        lause.setInt(1, kysymyksenIndeksi);
+        ResultSet rs = lause.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+    }
+
+    private static int tulostaKysymysvaihtoehdot(Connection con, int kysymyksenIndeksi) throws SQLException {
+        String sql2 = "SELECT * FROM kysymysvaihtoehdot WHERE kysymysID=?";
+        PreparedStatement lause2 = con.prepareStatement(sql2);
+        lause2.setInt(1, kysymyksenIndeksi);
+        ResultSet rs2 = lause2.executeQuery();
+        int k = 1;
+        int oikea = 0;
+        while (rs2.next()) {
+            System.out.println(k + ". " + rs2.getString(2));
+            if (rs2.getInt("oikeavastaus") == 1)
+                oikea = k;
+            k++;
+        }
+        return oikea;
     }
 }
